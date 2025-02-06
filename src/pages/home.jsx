e8b4272870265f784ce572553ab4e8317ea1dd61
Lauren Yraola <laurenyraola03@gmail.com>
2025-02-06 12:54:53 -0800

    basic web structure

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return(
+    <>
+        <h1>better days</h1>
+        <p>welcome back, lauren.</p>
+    </>
+    );
+};
+
+export default Home;
